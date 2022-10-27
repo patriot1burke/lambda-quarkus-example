0841190a1727d8b673a5c53bc1ec9f431676204c
Bill Burke <bburke@redhat.com>
2022-10-27 07:22:49 -0400

    initial commit

---
+++ manage.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+function cmd_create() {
+  echo Creating function
+  set -x
+  aws lambda-snapstart create-function \
+    --region us-east-2 \
+    --function-name ${FUNCTION_NAME} \
+    --code S3Bucket=billburke/function.zip \
+    --handler ${HANDLER} \
+    --runtime ${RUNTIME} \
+    --role ${LAMBDA_ROLE_ARN} \
+    --timeout 15 \
+    --memory-size 256 \
+    --snap-start ApplyOn=PublishedVersions \
+    ${LAMBDA_META}
+# Enable and move this param above ${LAMBDA_META}, if using AWS X-Ray
+#    --tracing-config Mode=Active \
+}
+
+function cmd_delete() {
+  echo Deleting function
+  set -x
+  aws lambda delete-function --function-name ${FUNCTION_NAME}
+}
+
+function cmd_publish() {
+  echo Deleting function
+  set -x
+  aws lambda publish-version --function-name ${FUNCTION_NAME}
+}
+
+function cmd_invoke() {
+  echo Invoking function
+
+  inputFormat=""
+  if [ $(aws --version | awk '{print substr($1,9)}' | cut -c1-1) -ge 2 ]; then inputFormat="--cli-binary-format raw-in-base64-out"; fi
+
+  set -x
+
+  aws lambda invoke response.txt \
+    ${inputFormat} \
+    --function-name ${FUNCTION_NAME} \
+    --region us-east-2 \
+    --payload file://payload.json \
+    --log-type Tail \
+    --query 'LogResult' \
+    --output text |  base64 --decode
+  { set +x; } 2>/dev/null
+  cat response.txt && rm -f response.txt
+}
+
+function cmd_update() {
+  echo Updating function
+  set -x
+  aws lambda-snapstart update-function-code \
+    --region us-east-2 \
+    --function-name ${FUNCTION_NAME} \
+    --snap-start ApplyOn=PublishedVersions \
+    --zip-file ${ZIP_FILE}
+}
+
+FUNCTION_NAME=Firecracker:1
+HANDLER=io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest
+RUNTIME=java11
+ZIP_FILE=fileb:///home/bburke/projects/quarkus/patriot1burke/code-with-quarkus/target/function.zip
+
+function usage() {
+  [ "_$1" == "_" ] && echo -e "\nUsage (JVM): \n$0 [create|delete|invoke]\ne.g.: $0 invoke"
+  [ "_$1" == "_" ] && echo -e "\nUsage (Native): \n$0 native [create|delete|invoke]\ne.g.: $0 native invoke"
+
+  [ "_" == "_`which aws 2>/dev/null`" ] && echo -e "\naws CLI not installed. Please see https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html"
+  [ ! -e $HOME/.aws/credentials ] && [ "_$AWS_ACCESS_KEY_ID" == "_" ] && echo -e "\naws configure not setup.  Please execute: aws configure"
+  [ "_$LAMBDA_ROLE_ARN" == "_" ] && echo -e "\nEnvironment variable must be set: LAMBDA_ROLE_ARN\ne.g.: export LAMBDA_ROLE_ARN=arn:aws:iam::123456789012:role/my-example-role"
+}
+
+if [ "_$1" == "_" ] || [ "$1" == "help" ]
+ then
+  usage
+fi
+
+if [ "$1" == "native" ]
+then
+  RUNTIME=provided
+  ZIP_FILE=fileb:///home/bburke/projects/quarkus/patriot1burke/code-with-quarkus/target/function.zip
+  FUNCTION_NAME=CodeWithQuarkusNative
+  LAMBDA_META="--environment Variables={DISABLE_SIGNAL_HANDLERS=true}"
+  shift
+fi
+
+while [ "$1" ]
+do
+  eval cmd_${1}
+  { set +x; } 2>/dev/null
+  shift
+done
+
